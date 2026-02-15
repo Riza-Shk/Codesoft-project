6d9fe45b8e3159211662b609dccdd9f7ea120ce1
Riza-Shk <riza.shaikh1000@gmail.com>
2026-02-16 00:23:03 +0530

    repairing the date

--- backend/models/History.js
+++ backend/models/History.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
-const historySchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
+const historySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    quizId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    quizTitle: String,
+    score: Number,
+    totalQuestions: Number,
+    takenAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  quizId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-  quizTitle: String,
-  score: Number,
-  totalQuestions: Number,
-  takenAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("History", historySchema);
